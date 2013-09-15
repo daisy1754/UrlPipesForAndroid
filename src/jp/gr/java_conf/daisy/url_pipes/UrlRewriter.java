d704298420c7aeab38369fe42a5790e47ec822cf
daisy1754 <d-part@hotmail.co.jp>
2013-09-15 15:27:53 +0900

    add rules for Hatena-bookmark

--- src/jp/gr/java_conf/daisy/url_pipes/UrlRewriter.java
+++ src/jp/gr/java_conf/daisy/url_pipes/UrlRewriter.java
@@ -2,11 +2,23 @@ package jp.gr.java_conf.daisy.url_pipes;
 
 import android.net.Uri;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class UrlRewriter {
-    private final Set<PipeRule> RULES = new HashSet<PipeRule>();
+    private final String HATENA_BOOKMARK_HTTPS_PREFIX = "http://b.hatena.ne.jp/entry.touch/s/";
+    private final String HATENA_BOOKMARK_PREFIX = "http://b.hatena.ne.jp/entry.touch/";
+    private final String HATENA_BOOKMARK_DESKTOP_HTTPS_PREFIX = "http://b.hatena.ne.jp/entry/s/";
+    private final String HATENA_BOOKMARK_DESKTOP_PREFIX = "http://b.hatena.ne.jp/entry/";
+    private final Set<PipeRule> RULES_FOR_HATENA_BOOKMARK
+            = new HashSet<PipeRule>(Arrays.asList(new PipeRule[]{
+            new ReplacePrefix(HATENA_BOOKMARK_HTTPS_PREFIX, "https://"),
+            new ReplacePrefix(HATENA_BOOKMARK_PREFIX, "http://"),
+            new ReplacePrefix(HATENA_BOOKMARK_DESKTOP_HTTPS_PREFIX, "https://"),
+            new ReplacePrefix(HATENA_BOOKMARK_DESKTOP_PREFIX, "http://"),
+    }));
+    private final Set<PipeRule> RULES = new HashSet<PipeRule>(RULES_FOR_HATENA_BOOKMARK);
 
     public Uri rewrite(Uri uri) {
         return Uri.parse(rewrite(uri.toString()));
